85d2a873dae87341ff05d57aa65dde506122ae1b
Anshuman Kundu <kundu4coding@gmail.com>
2023-09-19 23:17:10 +0530

    Complete Project Done

--- src/utils.py
+++ src/utils.py
@@ -31,4 +31,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
         return report
     
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e, sys)  # type: ignore
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys) # type: ignore
